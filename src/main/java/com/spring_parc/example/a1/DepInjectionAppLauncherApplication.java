9cb5b8d8be3d173d5ceea2b8817762d8e58b60dd
Leegeonwoo7 <workgeon07@naver.com>
2024-03-05 23:04:27 +0900

    필드 의존성 주입

--- src/main/java/com/spring_parc/example/a1/DepInjectionAppLauncherApplication.java
+++ src/main/java/com/spring_parc/example/a1/DepInjectionAppLauncherApplication.java
@@ -1,16 +1,49 @@
 package com.spring_parc.example.a1;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
+@Component
+class YourBusinessClass{
+    @Autowired
+    Dependency1 dependency1;
+    @Autowired
+    Dependency2 dependency2;
+
+    public void setDependency1(Dependency1 dependency1) {
+        this.dependency1 = dependency1;
+    }
+
+    public void setDependency2(Dependency2 dependency2) {
+        this.dependency2 = dependency2;
+    }
+
+    public String toString(){
+        return "Using " + dependency1 + " and "  + dependency2;
+    }
+}
+
+@Component
+class Dependency1{
+
+}
+
+@Component
+class Dependency2{
+
+}
 @Configuration
-@ComponentScan("com.spring_parc.example.a1")
+@ComponentScan
 public class DepInjectionAppLauncherApplication {
     public static void main(String[] args){
         var context = new AnnotationConfigApplicationContext(DepInjectionAppLauncherApplication.class);
         Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+
+        System.out.println(context.getBean(YourBusinessClass.class));
     }
 }
